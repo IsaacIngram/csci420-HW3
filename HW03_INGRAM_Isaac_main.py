d56b18035cbfe263a979f0a64f92c1a41fd4f347
IsaacIngram <me@isaacingram.dev>
2025-02-09 17:03:49 -0500

    Add TPR zero point, best threshold point, and FAR zero point

--- HW03_INGRAM_Isaac_main.py
+++ HW03_INGRAM_Isaac_main.py
@@ -41,6 +41,11 @@ def main():
     ideal_point = (1,0)
     ideal_point_distance = math.sqrt(2)
     ideal_point_speed = 0
+    # Initialize tracker for the first zero points for both false alarm rate and true positive rate
+    first_zero_far_point_speed = None
+    last_tpr_one_point_speed = None
+
+    least_total_error_point = (1, 0, 1, 0)
 
     # Iterate through all possible threshold speeds
     for threshold_speed in range(min_speed, max_speed + 1):
@@ -72,6 +77,11 @@ def main():
         # Calculate false negative rate and add it to the list
         fnr = num_false_positives / (num_false_positives + num_true_negatives)
         false_alarm_rates.append(fnr)
+        # Calculate total rate of mistakes for this threshold
+        percent_mistakes = (num_false_negatives + num_false_positives) / final_df.size
+
+        if percent_mistakes < least_total_error_point[2]:
+            least_total_error_point = (fnr, tpr, percent_mistakes, threshold_speed)
 
         # Calculate Euclidean distance of this point to (0,1)
         distance_from_ideal = math.sqrt((fnr ** 2) + ((tpr - 1) ** 2))
@@ -81,17 +91,33 @@ def main():
             ideal_point = (fnr, tpr)
             ideal_point_speed = threshold_speed
 
+        # Set as the first false alarm rate zero point if it hasn't already been set, and this point has a FAR of 0
+        if fnr == 0 and first_zero_far_point_speed is None:
+            first_zero_far_point_speed = (fnr, tpr, threshold_speed)
+
+        # Set as the first true positive rate zero point if it hasn't already been set, and this point has a TPR of 0
+        if tpr == 1:
+            last_tpr_one_point_speed = (fnr, tpr, threshold_speed)
+
     # Plot the graph of false alarm rate and true positive rate points
     plt.plot(false_alarm_rates, true_positive_rates, marker='o', linestyle='--')
-    plt.xlim(0,1)
-    plt.ylim(0,1)
     plt.ylabel("True Positive Rate")
     plt.xlabel("False Alarm Rate")
     plt.title("Receiver Operating Characteristic Curve for Threshold Speeds")
     # Add label and surrounding box for point closest to ideal point
-    plt.text(ideal_point[0] + 0.02, ideal_point[1] - 0.45, f'Point Closet to Ideal ({ideal_point_speed} mph)', color='purple', rotation=-45, ha='left')
+    plt.text(ideal_point[0] + 0.03, ideal_point[1] - 0.49, f'Point Closet to Ideal ({ideal_point_speed} mph)', color='purple', rotation=-45, ha='left')
     plt.plot(ideal_point[0], ideal_point[1], marker='s', mfc='none', mec='purple', markersize=10)
-    plt.show()
+    # Add label and surrounding box for first false alarm rate of zero point
+    plt.text(first_zero_far_point_speed[0] + 0.04, first_zero_far_point_speed[1] - 0.02, f'First False Alarm Rate of Zero ({first_zero_far_point_speed[2]} mph)', color='blue')
+    plt.plot(first_zero_far_point_speed[0], first_zero_far_point_speed[1], marker='s', mfc='none', mec='blue', markersize=10)
+    # Add label and surrounding box for first true positive rate of one point
+    plt.text(last_tpr_one_point_speed[0] - 0.02, last_tpr_one_point_speed[1] - 0.85, f'Last True Positive Rate of One ({last_tpr_one_point_speed[2]} mph)', rotation=-90, color='green')
+    plt.plot(last_tpr_one_point_speed[0], last_tpr_one_point_speed[1], marker='s', mfc='none', mec='green', markersize=10)
+    # Add label and surrounding box for point with the least number of mistakes
+    plt.text(least_total_error_point[0] + 0.01, least_total_error_point[1] - 0.57, f'Best threshold with lowest number\nof mistakes ({least_total_error_point[3]} mph)', rotation = -45, color='red')
+    plt.plot(least_total_error_point[0], least_total_error_point[1], marker='s', mfc='none', mec='red', markersize=10)
 
+    plt.grid()
+    plt.show()
 if __name__ == '__main__':
     main()
